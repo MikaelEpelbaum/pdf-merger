03c133c0d4a306d6941ee9b753a0ea1147ac5028
mikael epelbaum <mikaelepelbaum@gmail.com>
2020-04-07 10:12:26 +0300

    arranged front page

--- merger.py
+++ merger.py
@@ -13,7 +13,11 @@ for file in listdir(directory):
 
 files.sort(key=path.getctime)
 
-if not files[-1].startswith('Scan'):
+index = [i for i, s in enumerate(files) if 'Scan' not in s][0]
+
+files = files[index:index+1] + files[:index] + files[index+1:]
+
+if 'Scan' not in files[-1]:
     files = files[-1:] + files[:-1]
 
 pdf = Pdf.new()
